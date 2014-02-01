911ef3507f329faec425b2427b1fd02093dd7c2b
Christian <christian.chgp@gmail.com>
2014-02-01 17:48:33 +0000

    moved search to activity

--- VolleySink/src/main/java/chris/volleytest/SearchArtistActivity.java
+++ VolleySink/src/main/java/chris/volleytest/SearchArtistActivity.java
@@ -1,11 +1,32 @@
 package chris.volleytest;
 
 import android.app.Activity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
+import android.widget.Toast;
 
-public class SearchArtistActivity extends Activity {
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import chris.volleytest.network.SearchArtistRequest;
+import chris.volleytest.network.model.Artist;
+import chris.volleytest.network.model.ArtistResults;
+
+public class SearchArtistActivity extends Activity implements Response.Listener<ArtistResults>, Response.ErrorListener {
+
+    public static final String       TAG_FRAGMENT_SEARCH = "fragment_search";
+    private             RequestQueue mRequestQueue       = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,17 +35,36 @@ public class SearchArtistActivity extends Activity {
 
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
-                                .add(R.id.container, new SearchArtistFragment())
+                                .add(R.id.container, new SearchArtistFragment(), TAG_FRAGMENT_SEARCH)
                                 .commit();
         }
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        handleIntent(getIntent());
+
     }
 
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        handleIntent(intent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        @SuppressWarnings("ConstantConditions")
+        SearchView searchView = (SearchView) menu.findItem(R.id.search)
+                                                 .getActionView();
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        }
         return true;
     }
 
@@ -40,5 +80,43 @@ public class SearchArtistActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onStop() {
+        super.onStop();
+        mRequestQueue.cancelAll(new RequestQueue.RequestFilter() {
+            @Override
+            public boolean apply(Request<?> request) {
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError volleyError) {
+        Toast.makeText(this, "Error retrieving results" + volleyError, Toast.LENGTH_SHORT)
+             .show();
+    }
+
+    @Override
+    public void onResponse(ArtistResults artistResults) {
+        final List<Artist> results = artistResults.results != null ? artistResults.results : new ArrayList<Artist>();
+        updateResults(results);
+    }
+
+    private void handleIntent(Intent intent) {
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String searchQuery = intent.getStringExtra(SearchManager.QUERY);
+            final SearchArtistRequest searchArtistRequest = new SearchArtistRequest(searchQuery, this, this);
+            mRequestQueue.add(searchArtistRequest);
+
+        }
+    }
+
+    private void updateResults(List<Artist> results) {
+        final SearchArtistFragment searchFragment = (SearchArtistFragment) getFragmentManager().findFragmentByTag(TAG_FRAGMENT_SEARCH);
+        if (searchFragment != null) {
+            searchFragment.updateResults(results);
+        }
+    }
 
 }
--- VolleySink/src/main/java/chris/volleytest/SearchArtistFragment.java
+++ VolleySink/src/main/java/chris/volleytest/SearchArtistFragment.java
@@ -2,51 +2,29 @@ package chris.volleytest;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Context;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
-
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.Volley;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import chris.volleytest.network.SearchArtistRequest;
 import chris.volleytest.network.model.Artist;
-import chris.volleytest.network.model.ArtistResults;
 
 /**
  * Created by christian on 01/02/2014.
  * <p/>
  * Fragment to search artists
  */
-public class SearchArtistFragment extends Fragment implements Response.Listener<ArtistResults>, Response.ErrorListener {
+public class SearchArtistFragment extends Fragment {
 
-    private RequestQueue         mRequestQueue = null;
-    private ArrayAdapter<Artist> mListAdapter  = null;
-    private EditText             mEtSearch     = null;
-    private List<Artist>         mResults      = new ArrayList<Artist>();
+    private ArrayAdapter<Artist> mListAdapter = null;
+    private List<Artist>         mResults     = new ArrayList<Artist>();
 
     public SearchArtistFragment() {
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        mRequestQueue = Volley.newRequestQueue(activity);
-
     }
 
     @Override
@@ -59,24 +37,12 @@ public class SearchArtistFragment extends Fragment implements Response.Listener<
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         if (rootView != null) {
-            // edit text
-            mEtSearch = (EditText) rootView.findViewById(R.id.et_search);
-
-            //button search
-            final Button btnSearch = (Button) rootView.findViewById(R.id.btn_search);
-            btnSearch.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    onSearch_Clicked();
-                }
-            });
 
             // list view
             ListView listView = (ListView) rootView.findViewById(R.id.listView);
             final Activity activity = getActivity();
             if (activity != null) {
-                mListAdapter = new ArrayAdapter<Artist>(activity, android.R.layout.simple_list_item_1, android.R.id.text1);
-                updateListElements();
+                mListAdapter = new ArrayAdapter<Artist>(activity, android.R.layout.simple_list_item_1, android.R.id.text1, mResults);
                 listView.setAdapter(mListAdapter);
                 listView.setEmptyView(rootView.findViewById(android.R.id.empty));
             }
@@ -84,43 +50,8 @@ public class SearchArtistFragment extends Fragment implements Response.Listener<
         return rootView;
     }
 
-    private void onSearch_Clicked() {
-        final Editable text = mEtSearch.getText();
-        final String searchQuery = text != null ? text.toString() : "";
-        final SearchArtistFragment responseListener = this;
-        final SearchArtistRequest searchArtistRequest = new SearchArtistRequest(searchQuery, responseListener, responseListener);
-        mRequestQueue.add(searchArtistRequest);
-    }
-
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        mRequestQueue.cancelAll(new RequestQueue.RequestFilter() {
-            @Override
-            public boolean apply(Request<?> request) {
-                return true;
-            }
-        });
-    }
-
-    @Override
-    public void onErrorResponse(VolleyError volleyError) {
-        Context context = getActivity();
-        if (context != null) {
-            Toast.makeText(context, "Error retrieving results" + volleyError, Toast.LENGTH_SHORT)
-                 .show();
-        }
-    }
-
-    @Override
-    public void onResponse(ArtistResults artistResults) {
-        mResults = artistResults.results != null ? artistResults.results : new ArrayList<Artist>();
-        updateListElements();
-    }
-
-    private void updateListElements() {
-        mListAdapter.clear();
-        mListAdapter.addAll(mResults);
+    public void updateResults(List<Artist> results) {
+        mResults = new ArrayList<Artist>(results);
+        mListAdapter.notifyDataSetChanged();
     }
 }
