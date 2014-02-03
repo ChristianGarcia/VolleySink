633c60b48ebf060aabcc62530ab74a7379c33ca9
Christian Garcia <christian.chgp@gmail.com>
2014-02-03 23:47:01 +0000

    change app package

--- VolleySink/src/main/java/chris/volleysink/ArtistActivity.java
+++ VolleySink/src/main/java/chris/volleysink/ArtistActivity.java
@@ -9,7 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import chris.volleytest.R;
+import chris.volleysink.R;
 
 public class ArtistActivity extends Activity {
 
